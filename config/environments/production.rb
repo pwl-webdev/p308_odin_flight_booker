169054609570f44028e005701df15a24842715a4
Pwl Webdev <pwl.webdev@gmail.com>
2016-11-15 22:53:45 +0100

    using letter opener on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,15 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "p308_odin_flight_booker_#{Rails.env}"
+  #config.action_mailer.perform_caching = false
+
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :letter_opener
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
